4ecbdada28efd097520dfe5dfb59c21ebb84315f
Yves Vinckier <yves.vinckier@live.fr>
2019-08-03 23:34:36 +0200

    First commit

---
+++ gatsby-theme-events/.cache/async-requires.js
@@ -0,0 +1,7 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/yvesvinckier/authoring-gatsby-theme/gatsby-theme-events/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
+}
+
